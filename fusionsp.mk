2c44ff5192cc5f5c1769a60b1372643dd273dfe5
Steve Kondik <steve@cyngn.com>
2015-06-14 22:35:07 -0700

    shamu: Define TARGET_VENDOR
    Change-Id: I7d6d4377d9486750951b510b7be5e5e4b5088bdb

--- fusionsp.mk
+++ fusionsp.mk
@@ -19,6 +19,8 @@ PRODUCT_NAME := shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
+TARGET_VENDOR := moto
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
     BUILD_FINGERPRINT=google/shamu/shamu:5.1.1/LMY48M/2167285:user/release-keys \
