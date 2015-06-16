396f0bf884ef80f1c2f1fcec9fbcb2b2499b7d61
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-06-16 15:50:06 +0200

    Update fusion make file.
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- fusionsp.mk
+++ fusionsp.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-fusion
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := fusionsp_shamu
+PRODUCT_NAME := shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
