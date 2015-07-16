f8c1ff3f195ca29f89dbf6f483ab0046880c9b6e
Zyg0te <edvard.holst@gmail.com>
2015-07-16 11:02:32 +0200

    Corrected the boot animation screen dimention parameters
    -TARGET_SCREEN_HEIGHT and TARGET_SCREEN_WIDTH was seemingly flipped.
    
    Change-Id: I176c837362fcd4e5d6bb62211c30f4e2f034f48d

--- fusionsp.mk
+++ fusionsp.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1440
-TARGET_SCREEN_WIDTH := 2560
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common Fusion stuff.
 $(call inherit-product, vendor/fusion/config/common_phone.mk)
